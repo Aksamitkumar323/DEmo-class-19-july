7b9d39f5ac4033b756a13d453e7ca3cfd7a65d64
Amit Kumar <amit.official323@gmail.com>
2022-07-20 10:37:32 +0530

    Altered abc.java

--- abc.java
+++ abc.java
@@ -2,6 +2,7 @@ public class Main {
   public static void main(String[] args) {
     for (int i = 0; i <= 10; i++) {
       System.out.println("Hello EveryBody",+i);
-    }  
+    }
+     System.out.println("thanks");
   }
 }
